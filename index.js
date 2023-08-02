c444559caa670a89d5d7beb20e2a85fc1b433c8e
Jorge Eduardo Ardila <jea.data@gmail.com>
2023-08-02 17:10:51 -0500

    http server parameters

--- index.js
+++ index.js
@@ -1,31 +1,40 @@
-'use strict';
+import * as http from 'http';
 
-import fs from 'fs';
-import {parse} from 'csv-parse';
+const PORT = 3000;
 
-const filePath = 'data/kepler_data.csv';
-const habitablePlanets = [];
+const server = http.createServer();
 
-const isHabitablePlanet = (planet) => {
-  return planet['koi_disposition'] === 'CONFIRMED' && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11 && planet['koi_prad'] < 1.6;
-}
+server.on('request', (req, res) => {
+  if (req.url === '/friends') {
+    res.writeHead(200, {
+      'Content-Type': 'text/json',
+    });
+    res.end(JSON.stringify({
+      id: 1, name: 'Jorge Eduardo', status: true, role: 'Backend Developer', hobbie: 'runninG'
+    }));
+  } else if (req.url === '/messages') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<body>');
+    res.write('<h1>Messages List</h1>');
+    res.write('<div>');
+    res.write('<ul>');
+    res.write('<li>Consider donating for our cause!</li>');
+    res.write('<li>Keep away of unsolicited donation requests!</li>');
+    res.write('<li>travelling to caribbean islands is pleasant</li>');
+    res.write('<li>Don\'t forget to ask for your degree certificate</li>');
+    res.write('</ul>');
+    res.write('</div>');
+    res.write('</body>');
+    res.write('</html>');
+    res.end();
+  } else {
+    res.statusCode = 404;
+    res.end();
+  }
+});
 
-
-fs.createReadStream(filePath)
-  .pipe(parse({
-    comment: '#',
-    columns: true,
-  }))
-  .on('data', (data) => {
-    if (isHabitablePlanet(data)) {
-      habitablePlanets.push(data);
-    }
-  })
-  .on('error', (err) => {
-    console.error(`[Error] : ${err}`);
-  })
-  .on('end', () => {
-    console.log(habitablePlanets.map((planet) => planet.kepler_name));
-    console.log(`${habitablePlanets.length} habitable planets found!`);
-  });
-
+server.listen(PORT, () => {
+  console.log(`[SERVER]: Running on localhost:${PORT}`);
+});
