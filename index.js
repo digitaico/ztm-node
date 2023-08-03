073e9615660741d88dd99ffc809a916115a174f0
Jorge Eduardo Ardila <jea.data@gmail.com>
2023-08-03 16:47:10 -0500

    vanilla http routes

--- index.js
+++ index.js
@@ -4,15 +4,34 @@ const PORT = 3000;
 
 const server = http.createServer();
 
+const friends = [
+  {id: 0, name: 'Charles Munger'},
+  {id: 1, name: 'Warren Buffet'},
+  {id: 2, name: 'Benjamin Graham'},
+  {id: 4, name: 'Steven Cohen'}
+];
+
 server.on('request', (req, res) => {
-  if (req.url === '/friends') {
+  const items = req.url.split('/');
+
+  if (req.method === 'POST' && items[1] === 'friends') {
+    req.on('data', (data) => {
+      const friend = data.toString();
+      //console.log('Request: ', friend);
+      friends.push(JSON.parse(friend));
+    });
+    req.pipe(res);
+  } else if (req.method === 'GET' && items[1] === 'friends') {
     res.writeHead(200, {
       'Content-Type': 'text/json',
     });
-    res.end(JSON.stringify({
-      id: 1, name: 'Jorge Eduardo', status: true, role: 'Backend Developer', hobbie: 'runninG'
-    }));
-  } else if (req.url === '/messages') {
+    if (items.length === 3) {
+      const friendIndex = Number(items[2]);
+      res.end(JSON.stringify(friends[friendIndex]))
+    } else {
+      res.end(JSON.stringify(friends));
+    }
+  } else if (req.method === 'GET' && items[1] === 'messages') {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
